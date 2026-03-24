dff632ea58a31175e90356c0990cfab5a38337c6
ratman <szczuras7@proton.me>
2026-03-24 19:57:17 +0100

    3.5 bubble sort

--- zad1/zad1.pas
+++ zad1/zad1.pas
@@ -11,9 +11,16 @@ end;
 
 procedure bubbleSort(var arr: array of Integer);
 var
-    i, j: Integer;
+    i, j, temp: Integer;
 begin
-
+    for i := Low(arr) to High(arr) - 1 do
+        for j:= Low(arr) to High(arr) - 1 - i do
+            if arr[j] > arr[j + 1] then
+            begin
+                temp := arr[j];
+                arr[j] := arr[j + 1];
+                arr[j + 1] := temp;
+            end;
 end;
 
 var
@@ -22,6 +29,7 @@ var
 
 begin
     generateRandom(numbers);
+    bubbleSort(numbers);
 
     for i := Low(numbers) to High(numbers) do
         WriteLn(numbers[i]);
